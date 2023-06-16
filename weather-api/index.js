67598fea278b929b06f158f89b2cea22d03761df
Jessica Bealey <jfbealey@gmail.com>
2023-06-16 14:30:20 +0100

    Weather api call that returns temp and main values

---
+++ weather-api/index.js
@@ -0,0 +1,15 @@
+const apiKey = '1c752e5761aa229c70f540478666d1c6';
+const city = 'London';
+const apiUrl = `http://api.openweathermap.org/data/2.5/weather?units=metric&q=${city}&appid=${apiKey}`;
+
+let weatherData = null;
+
+fetch(apiUrl)
+  .then((response) => response.json())
+  .then((weatherData) => {
+    console.log(weatherData.main.temp, weatherData.weather[0].main)
+
+
+  });
+
+  console.log('Requesting weather data');
